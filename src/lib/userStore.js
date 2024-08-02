c6191584e8a9625a9f2c2822ba195ddd8067d54a
Prashant-jakhad <97466823+Prashant01102004@users.noreply.github.com>
2024-08-02 06:40:36 +0530

    Update userStore.js

--- src/lib/userStore.js
+++ src/lib/userStore.js
@@ -1,28 +1,25 @@
 import { doc, getDoc } from 'firebase/firestore';
-import { create } from 'zustand';
+import { create } from 'zustand'
 import { db } from './firebase';
 
 export const useUserStore = create((set) => ({
   currentUser: null,
-  isLoading: true,
-  fetchUserInfo: async (uid) => {
-    if (!uid) return set({ currentUser: null, isLoading: false });
-
+  isLoading:true,
+  fetchUserInfo: async (uid)=>{
+    if(!uid)return set({currentUser: null,isLoading:false});
     try {
-      console.log('Fetching user info for UID:', uid); // Log fetching attempt
-      const docRef = doc(db, "users", uid);
-      const docSnap = await getDoc(docRef);
+        const docRef= doc(db,"users",uid);
+        const docSnap=await getDoc(docRef);
 
-      if (docSnap.exists()) {
-        console.log('User Data:', docSnap.data()); // Log user data
-        set({ currentUser: docSnap.data(), isLoading: false });
-      } else {
-        console.log('No user found'); // Log if user does not exist
-        set({ currentUser: null, isLoading: false });
-      }
+        if(docSnap.exists()){
+            set({currentUser: docSnap.data(),isLoading:false});
+        }
+        else {
+            set({currentUser: null,isLoading:false});
+        }
     } catch (error) {
-      console.log(error);
-      set({ currentUser: null, isLoading: false });
+        console.log(error);
+        return set({currentUser: null,isLoading:false});
     }
-  },
-}));
+  }
+}))
