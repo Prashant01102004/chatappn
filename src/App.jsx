5f3fe23a78093a674222b68dd25cceeab237709e
Prashant-jakhad <97466823+Prashant01102004@users.noreply.github.com>
2024-08-02 06:40:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,46 @@
-import Chat from './components/chat/Chat';
-import List from './components/list/List';
-import Detail from './components/details/Detail';
-import Login from './components/login/Login';
-import Notification from './components/notification/Notification';
-import { useEffect, useCallback } from 'react';
-import { auth } from './lib/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
-import { useUserStore } from './lib/userStore';
-import { useChatStore } from './lib/chatStore';
+import Chat from './components/chat/Chat'
+import List from './components/list/List'
+import Detail from './components/details/Detail'
+import Login from './components/login/Login'
+import Notification from './components/notification/Notification'
+import { useEffect } from 'react'
+import { auth} from './lib/firebase';
+import { onAuthStateChanged } from 'firebase/auth'
+import { useUserStore } from './lib/userStore'
+import { useChatStore } from './lib/chatStore'
 
 const App = () => {
-  const { chatId } = useChatStore();
-  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
-
-  // Memoize fetchUserInfo to ensure it doesn't change between renders
-  // const fetchUser = useCallback(fetchUserInfo, [fetchUserInfo]);
-
-  useEffect(() => {
-    console.log('Setting up auth state listener');
-    // Listen for changes to the user's authentication state.
-    const unSub = onAuthStateChanged(auth, (user) => {
-      console.log('Auth state changed:', user);
-      fetchUserInfo(user ? user.uid : null);
+  const {chatId}=useChatStore();
+  const {currentUser,isLoading,fetchUserInfo}=useUserStore()
+  
+  useEffect(()=>{
+    const unSub=onAuthStateChanged(auth,(user)=>{
+      fetchUserInfo(user?.uid);
     });
-
-    return () => {
-      console.log('Cleaning up auth state listener');
+    return ()=>{
       unSub();
     };
-  }, [fetchUserInfo]);
-
-  console.log('Current User:', currentUser);
-  console.log('Is Loading:', isLoading);
-
-  if (isLoading) return <div className="loading">Loading...</div>;
-
+  },[fetchUserInfo]);
+  
+  console.log(currentUser);
+  
+  if(isLoading)return <div className='loading'>Loading....</div>
   return (
-    <div className="container">
+    <div className='container'>
       {currentUser ? (
         <>
-          <List />
-          {chatId && <Chat />}
-          {chatId && <Detail />}
+        <List/>
+        {chatId && <Chat/>}
+        {chatId && <Detail/>}
         </>
       ) : (
-        <Login />
+        <>
+        <Login/>
+        </>
       )}
-      <Notification />
+      <Notification/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
