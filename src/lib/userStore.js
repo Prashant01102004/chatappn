f913005378cb400e41553379abcd0512f19ebf98
Prashant-jakhad <97466823+Prashant01102004@users.noreply.github.com>
2024-08-02 06:21:38 +0530

    Update userStore.js

--- src/lib/userStore.js
+++ src/lib/userStore.js
@@ -1,25 +1,24 @@
 import { doc, getDoc } from 'firebase/firestore';
-import { create } from 'zustand'
+import { create } from 'zustand';
 import { db } from './firebase';
 
 export const useUserStore = create((set) => ({
   currentUser: null,
-  isLoading:true,
-  fetchUserInfo: async (uid)=>{
-    if(!uid)return set({currentUser: null,isLoading:false});
+  isLoading: true,
+  fetchUserInfo: async (uid) => {
+    if (!uid) return set({ currentUser: null, isLoading: false });
     try {
-        const docRef= doc(db,"users",uid);
-        const docSnap=await getDoc(docRef);
+      const docRef = doc(db, "users", uid);
+      const docSnap = await getDoc(docRef);
 
-        if(docSnap.exists()){
-            set({currentUser: docSnap.data(),isLoading:false});
-        }
-        else {
-            set({currentUser: null,isLoading:false});
-        }
+      if (docSnap.exists()) {
+        set({ currentUser: docSnap.data(), isLoading: false });
+      } else {
+        set({ currentUser: null, isLoading: false });
+      }
     } catch (error) {
-        console.log(error);
-        return set({currentUser: null,isLoading:false});
+      console.log(error);
+      set({ currentUser: null, isLoading: false });
     }
-  }
-}))
+  },
+}));
